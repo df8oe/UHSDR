ec05f359b7f5481ae2ea29bc19a43bb5faf706e1
hogthrob <hogthrob@github.com>
2016-01-21 23:27:01 +0100

    Fixed regression again

--- mchf-eclipse/drivers/ui/oscillator/ui_si570.c
+++ mchf-eclipse/drivers/ui/oscillator/ui_si570.c
@@ -390,97 +390,99 @@ static uchar ui_si570_change_frequency(float new_freq, uchar test)
       	}
    	}
 
-   	CriticalError(102);
-
-found:
-
-	//printf("(%d) %d %d\n\r",curr_div,n1,hsdiv);
-
- 	// New RFREQ calculation
-	os.rfreq = ((long double)new_freq * (long double)(n1 * hsdiv)) / os.fxtal;
-   	//printf("%d\n\r",(int)os.rfreq);
-
-   	// Debug print calc freq
-   	//printf("%d\n\r",(int)((os.fxtal*os.rfreq)/(n1*hsdiv)));
-
-#ifdef LOWER_PRECISION
-   	ratio = new_freq / fout0;
-   	ratio = ratio * (((float)n1)/((float)os.init_n1));
-   	ratio = ratio * (((float)hsdiv)/((float)os.init_hsdiv));
-   	final_rfreq_long = ratio * os.init_rfreq;
-#endif
-
-   	for(i = 0; i < 6; i++)
-   		os.regs[i] = 0;
-
-   	hsdiv = hsdiv - 4;
-   	os.regs[0] = (hsdiv << 5);
-
-   	if(n1 == 1)
-     	n1 = 0;
-   	else if((n1 & 1) == 0)
-      	n1 = n1 - 1;
-
-   	os.regs[0] = ui_si570_setbits(os.regs[0], 0xE0, (n1 >> 2));
-   	os.regs[1] = (n1 & 3) << 6;
-
-#ifdef LOWER_PRECISION
-   	os.regs[1] = os.regs[1] | (final_rfreq_long >> 30);
-   	os.regs[2] = final_rfreq_long >> 22;
-   	os.regs[3] = final_rfreq_long >> 14;
-   	os.regs[4] = final_rfreq_long >> 6;
-   	os.regs[5] = final_rfreq_long << 2;
-#endif
-
-#ifdef HIGHER_PRECISION
-   	whole = floorf(os.rfreq);
-   	frac_bits = floorf((os.rfreq - whole) * POW_2_28);
-
-   	for(i = 5; i >= 3; i--)
-   	{
-   		os.regs[i] = frac_bits & 0xFF;
-   		frac_bits = frac_bits >> 8;
+   	if (!found) {
+   		CriticalError(102);
    	}
+   	else
+   	{
 
-   	os.regs[2] = ui_si570_setbits(os.regs[2], 0xF0, (frac_bits & 0xF));
-   	os.regs[2] = ui_si570_setbits(os.regs[2], 0x0F, (whole & 0xF) << 4);
-   	os.regs[1] = ui_si570_setbits(os.regs[1], 0xC0, (whole >> 4) & 0x3F);
-#endif
+		//printf("(%d) %d %d\n\r",curr_div,n1,hsdiv);
 
-   	//printf("write %02x %02x %02x %02x %02x %02x\n\r",os.regs[0],os.regs[1],os.regs[2],os.regs[3],os.regs[4],os.regs[5]);
+		// New RFREQ calculation
+		os.rfreq = ((long double)new_freq * (long double)(n1 * hsdiv)) / os.fxtal;
+		//printf("%d\n\r",(int)os.rfreq);
 
-   	// check to see if this tuning will result in a "large" tuning step, without setting the frequency
-   	if(test)
-   		return(ui_si570_is_large_change());
+		// Debug print calc freq
+		//printf("%d\n\r",(int)((os.fxtal*os.rfreq)/(n1*hsdiv)));
 
-   	//
-	if(ui_si570_is_large_change())
-	{
-		res = ui_si570_large_frequency_change();
-	}
-	else
-	{
-		res = ui_si570_small_frequency_change();
+	#ifdef LOWER_PRECISION
+		ratio = new_freq / fout0;
+		ratio = ratio * (((float)n1)/((float)os.init_n1));
+		ratio = ratio * (((float)hsdiv)/((float)os.init_hsdiv));
+		final_rfreq_long = ratio * os.init_rfreq;
+	#endif
 
-		// Maybe large change was needed, let's try it
-		if(res)	{
+		for(i = 0; i < 6; i++)
+			os.regs[i] = 0;
+
+		hsdiv = hsdiv - 4;
+		os.regs[0] = (hsdiv << 5);
+
+		if(n1 == 1)
+			n1 = 0;
+		else if((n1 & 1) == 0)
+			n1 = n1 - 1;
+
+		os.regs[0] = ui_si570_setbits(os.regs[0], 0xE0, (n1 >> 2));
+		os.regs[1] = (n1 & 3) << 6;
+
+	#ifdef LOWER_PRECISION
+		os.regs[1] = os.regs[1] | (final_rfreq_long >> 30);
+		os.regs[2] = final_rfreq_long >> 22;
+		os.regs[3] = final_rfreq_long >> 14;
+		os.regs[4] = final_rfreq_long >> 6;
+		os.regs[5] = final_rfreq_long << 2;
+	#endif
+
+	#ifdef HIGHER_PRECISION
+		whole = floorf(os.rfreq);
+		frac_bits = floorf((os.rfreq - whole) * POW_2_28);
+
+		for(i = 5; i >= 3; i--)
+		{
+			os.regs[i] = frac_bits & 0xFF;
+			frac_bits = frac_bits >> 8;
+		}
+
+		os.regs[2] = ui_si570_setbits(os.regs[2], 0xF0, (frac_bits & 0xF));
+		os.regs[2] = ui_si570_setbits(os.regs[2], 0x0F, (whole & 0xF) << 4);
+		os.regs[1] = ui_si570_setbits(os.regs[1], 0xC0, (whole >> 4) & 0x3F);
+	#endif
+
+		//printf("write %02x %02x %02x %02x %02x %02x\n\r",os.regs[0],os.regs[1],os.regs[2],os.regs[3],os.regs[4],os.regs[5]);
+
+		// check to see if this tuning will result in a "large" tuning step, without setting the frequency
+		if(test)
+			return(ui_si570_is_large_change());
+
+		//
+		if(ui_si570_is_large_change())
+		{
 			res = ui_si570_large_frequency_change();
 		}
-	}
+		else
+		{
+			res = ui_si570_small_frequency_change();
 
-	if(res)
-		return res;
+			// Maybe large change was needed, let's try it
+			if(res)	{
+				res = ui_si570_large_frequency_change();
+			}
+		}
 
-	// Verify second time - we might be transmitting, so
-	// it is absolutely unacceptable to be on startup
-	// SI570 frequency if any I2C error or chip reset occurs!
-	res = ui_si570_verify_frequency();
-	if(res == 0)
-		os.rfreq_old = os.rfreq;
+		if(res)
+			return res;
 
-	//if(res)
-	//	printf("---- error ----\n\r");
+		// Verify second time - we might be transmitting, so
+		// it is absolutely unacceptable to be on startup
+		// SI570 frequency if any I2C error or chip reset occurs!
+		res = ui_si570_verify_frequency();
+		if(res == 0)
+			os.rfreq_old = os.rfreq;
 
+		//if(res)
+		//	printf("---- error ----\n\r");
+   	}
 	return res;
 }
 
