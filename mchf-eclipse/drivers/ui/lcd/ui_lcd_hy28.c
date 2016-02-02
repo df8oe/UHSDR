d83d3148c9059b158fb21b8c78eaa1e1c3a6729c
df8oe <df8oe@gmx.de>
2016-02-02 18:53:22 +0100

    adding GitHub versioning string

--- mchf-eclipse/drivers/ui/lcd/ui_lcd_hy28.c
+++ mchf-eclipse/drivers/ui/lcd/ui_lcd_hy28.c
@@ -1758,6 +1758,11 @@ void UiLcdHy28_ShowStartUpScreen(ulong hold_time)
    sprintf(tx,"v %d.%d.%d.%d",TRX4M_VER_MAJOR,TRX4M_VER_MINOR,TRX4M_VER_RELEASE,TRX4M_VER_BUILD);
    UiLcdHy28_PrintText(110,80,tx,Grey3,Black,0);
 
+   // Show fourth line
+//   sprintf(tx,"DF8OE-GitHub-Version %s",$Id$);
+   sprintf(tx,"DF8OE-GitHub-Version %s","0.12.rt.4");
+   UiLcdHy28_PrintText(60,100,tx,Yellow,Black,0);
+
    //
    Read_EEPROM(EEPROM_FREQ_CONV_MODE, &i);	// get setting of frequency translation mode
 
--- mchf-eclipse/hardware/mchf_board.h
+++ mchf-eclipse/hardware/mchf_board.h
@@ -40,7 +40,7 @@
 //
 // -----------------------------------------------------------------------------
 #define		DEVICE_STRING			"mcHF QRP Transceiver"
-#define 	AUTHOR_STRING   		"K Atanassov - M0NKA 2014-2015"
+#define 	AUTHOR_STRING   		"K Atanassov - M0NKA 2014-2016"
 //
 #define 	TRX4M_VER_MAJOR			0
 #define 	TRX4M_VER_MINOR			219
