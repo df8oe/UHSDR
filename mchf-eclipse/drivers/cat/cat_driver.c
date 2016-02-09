36fc23617fa6025b8290839e9f948616d58a4f89
hogthrob <hogthrob@github.com>
2016-02-09 11:51:50 +0100

    Changed connection handling, buffer is reset when USB is disconnected. Should avoid sync issues hopefully. Still need to implement invalidation of buffer after some time to be even more safe here.

--- mchf-eclipse/drivers/cat/cat_driver.c
+++ mchf-eclipse/drivers/cat/cat_driver.c
@@ -21,6 +21,7 @@
 #include "usb_conf.h"
 #include "usbd_desc.h"
 #include "usbd_cdc_vcp.h"
+#include "usbd_audio_core.h"
 #include "cat_driver.h"
 #include "ui_driver.h"
 
@@ -49,7 +50,7 @@ void cat_driver_init(void)
 	USBD_Init(	&USB_OTG_dev,
 			USB_OTG_FS_CORE_ID,
 			&USR_desc,
-			&USBD_CDC_cb,
+			/* &USBD_CDC_cb*/ &AUDIO_cb,
 			&USR_cb);
 
 	printf("cat driver started\n\r");
@@ -100,6 +101,10 @@ int cat_buffer_add(uint8_t c) {
 	}
 }
 
+void cat_buffer_reset() {
+	cat_tail = cat_head;
+}
+
 
 uint8_t cat_driver_get_data(uint8_t* Buf,uint32_t Len) {
 	uint8_t res = 0;
@@ -125,6 +130,7 @@ uint8_t cat_driver_has_data() {
 	int32_t len = cat_head - cat_tail;
 	return len < 0?len+CAT_BUFFER_SIZE:len;
 }
+
 
 // #define DEBUG_FT817
 
@@ -192,150 +198,156 @@ struct FT817 ft817;
 void CatDriverFT817CheckAndExecute() {
 	uint8_t bc = 0;
 	uint8_t resp[32];
-	while (cat_driver_get_data(ft817.req,5))
-	{
+
+	if (cat_driver_state() == 0) {
+		cat_buffer_reset();
+	} else {
+
+		while (cat_driver_get_data(ft817.req,5))
+		{
 #ifdef DEBUG_FT817
-		int debug_idx;
-		for (debug_idx = 0; debug_idx < 5 && ft817.cmd_cntr < FT817_MAX_CMD; debug_idx++ ) {
-			ft817.reqs[ft817.cmd_cntr*5+debug_idx] = ft817.req[debug_idx];
-		}
-		ft817.cmd_cntr++;
+			int debug_idx;
+			for (debug_idx = 0; debug_idx < 5 && ft817.cmd_cntr < FT817_MAX_CMD; debug_idx++ ) {
+				ft817.reqs[ft817.cmd_cntr*5+debug_idx] = ft817.req[debug_idx];
+			}
+			ft817.cmd_cntr++;
 #endif
 
-		switch(ft817.req[4]) {
-		case 1: /* SET FREQ */
-		{
-			ulong f = 0;
-			int fidx;
-			for (fidx = 0; fidx < 4; fidx++) {
-				f *= 100;
-				f +=  (ft817.req[fidx] >> 4) * 10 + (ft817.req[fidx] & 0x0f);
-			}
-			f *= 40;
-			df.tune_new = f;
-			UiDriverUpdateFrequency(true,0);
-			resp[0] = 0;
-			bc = 1;
-		}
-		break;
-
-		case 3: /* READ FREQ */
-		{
-			ulong f = (df.tune_new + 20)/ 40 ;
-			ulong fbcd = 0;
-			int fidx;
-			for (fidx = 0; fidx < 8; fidx++)
+			switch(ft817.req[4]) {
+			case 1: /* SET FREQ */
 			{
-				fbcd >>= 4;
-				fbcd |= (f % 10) << 28;
-				f = f / 10;
+				ulong f = 0;
+				int fidx;
+				for (fidx = 0; fidx < 4; fidx++) {
+					f *= 100;
+					f +=  (ft817.req[fidx] >> 4) * 10 + (ft817.req[fidx] & 0x0f);
+				}
+				f *= 40;
+				df.tune_new = f;
+				UiDriverUpdateFrequency(true,0);
+				resp[0] = 0;
+				bc = 1;
 			}
+			break;
 
-			resp[0] = (uint8_t)(fbcd >> 24);
-			resp[1] = (uint8_t)(fbcd >> 16);
-			resp[2] = (uint8_t)(fbcd >> 8);
-			resp[3] = (uint8_t)fbcd;
-		}
-		switch(ts.dmod_mode) {
-		case DEMOD_LSB: resp[4] = 0; break;
-		case DEMOD_USB: resp[4] = 1; break;
-		case DEMOD_CW: 	resp[4] = 2; break;
-		case DEMOD_AM:  resp[4] = 4; break;
-		case DEMOD_FM:  resp[4] = 8; break;
-		default: resp[4] = 1;
-		}
-		bc = 5;
-		break;
-		case 7: /* set mode */
-		{
-			uint32_t new_mode = ts.dmod_mode;
-			switch (ft817.req[0]) {
-			case 0: // LSB
-				new_mode = DEMOD_LSB;
-				break;
-			case 1: // USB
-				new_mode = DEMOD_USB;
-				break;
-			case 2: // CW
-			case 3: // CW-R
-				new_mode = DEMOD_CW;
-				break;
-			case 4: // AM
-				new_mode = DEMOD_AM;
-				break;
-			case 8: // FM
-			case 0x88: // FM-N
-				new_mode = DEMOD_FM;
-				break;
-			case 0x0a: // DIG - SSB, side band controlled by some menu configuration in ft817, we use USB here
-				new_mode = DEMOD_USB;
-			case 0x0c: // PKT - FM, 9k6
-				new_mode = DEMOD_FM;
-				break;
+			case 3: /* READ FREQ */
+			{
+				ulong f = (df.tune_new + 20)/ 40 ;
+				ulong fbcd = 0;
+				int fidx;
+				for (fidx = 0; fidx < 8; fidx++)
+				{
+					fbcd >>= 4;
+					fbcd |= (f % 10) << 28;
+					f = f / 10;
+				}
+
+				resp[0] = (uint8_t)(fbcd >> 24);
+				resp[1] = (uint8_t)(fbcd >> 16);
+				resp[2] = (uint8_t)(fbcd >> 8);
+				resp[3] = (uint8_t)fbcd;
 			}
-			if  (new_mode != ts.dmod_mode) {
-				UiDriverSetDemodMode(new_mode);
+			switch(ts.dmod_mode) {
+			case DEMOD_LSB: resp[4] = 0; break;
+			case DEMOD_USB: resp[4] = 1; break;
+			case DEMOD_CW: 	resp[4] = 2; break;
+			case DEMOD_AM:  resp[4] = 4; break;
+			case DEMOD_FM:  resp[4] = 8; break;
+			default: resp[4] = 1;
+			}
+			bc = 5;
+			break;
+			case 7: /* set mode */
+			{
+				uint32_t new_mode = ts.dmod_mode;
+				switch (ft817.req[0]) {
+				case 0: // LSB
+					new_mode = DEMOD_LSB;
+					break;
+				case 1: // USB
+					new_mode = DEMOD_USB;
+					break;
+				case 2: // CW
+				case 3: // CW-R
+					new_mode = DEMOD_CW;
+					break;
+				case 4: // AM
+					new_mode = DEMOD_AM;
+					break;
+				case 8: // FM
+				case 0x88: // FM-N
+					new_mode = DEMOD_FM;
+					break;
+				case 0x0a: // DIG - SSB, side band controlled by some menu configuration in ft817, we use USB here
+					new_mode = DEMOD_USB;
+				case 0x0c: // PKT - FM, 9k6
+					new_mode = DEMOD_FM;
+					break;
+				}
+				if  (new_mode != ts.dmod_mode) {
+					UiDriverSetDemodMode(new_mode);
+				}
+			}
+			break;
+			case 8: /* PTT ON */
+				if(!ts.tx_disable)    {
+					ts.ptt_req = 1;
+					kd.enabled = 1;
+				}
+				resp[0] = 0; /* 0xF0 if PTT was already on */
+				bc = 1;
+				break;
+			case 15:
+				resp[0] = 0;
+				bc = 1;
+				break;
+			case 129: // SWITCH VFO
+				resp[0] = 0;
+				bc = 1;
+				break;
+			case 136: /* 0x88 PTT OFF */
+				resp[0] = 0; /* 0xF0 if PTT was already off */
+				ts.ptt_req = 0;
+				kd.enabled = 0;
+				bc = 1;
+				break;
+			case 167: /* A7 */
+				resp[0]=0xA7;
+				resp[1]=0x02; resp[2]=0x00;
+				resp[3]=0x04; resp[4]=0x67;
+				resp[5]=0xD8; resp[6]=0xBF;
+				resp[7]=0xD8; resp[8]=0xBF;
+				bc = 9;
+				break;
+			case 187: /* BB */
+				resp[0]=0x00;
+				resp[1]=0x00;
+				resp[2]=0x00;
+				resp[3]=0x00;
+				bc = 4;
+				break;
+			case 188: /* BC Write EEPROM */
+				resp[0] = 0;
+				bc = 1;
+			case 189: /* BD Read TX Status */
+				resp[0] = 0;
+				bc = 1;
+			case 231: /* E7 */
+				resp[0] = 0x09; // S9 signal;
+				bc = 1;
+				break;
+			case 247: /* F7 */
+				resp[0]=0x00;
+				bc = 1;
+				break;
+			case 255: /* FF sent out by HRD */
+				break;
+				// default:
+				// while (1);
 			}
 		}
-		break;
-		case 8: /* PTT ON */
-			if(!ts.tx_disable)    {
-				ts.ptt_req = 1;
-				kd.enabled = 1;
-			}
-			resp[0] = 0; /* 0xF0 if PTT was already on */
-			bc = 1;
-			break;
-		case 15:
-			resp[0] = 0;
-			bc = 1;
-			break;
-		case 129: // SWITCH VFO
-			resp[0] = 0;
-			bc = 1;
-			break;
-		case 136: /* 0x88 PTT OFF */
-			resp[0] = 0; /* 0xF0 if PTT was already off */
-			ts.ptt_req = 0;
-			kd.enabled = 0;
-			bc = 1;
-			break;
-		case 167: /* A7 */
-			resp[0]=0xA7;
-			resp[1]=0x02; resp[2]=0x00;
-			resp[3]=0x04; resp[4]=0x67;
-			resp[5]=0xD8; resp[6]=0xBF;
-			resp[7]=0xD8; resp[8]=0xBF;
-			bc = 9;
-			break;
-		case 187: /* BB */
-			resp[0]=0x00;
-			resp[1]=0x00;
-			resp[2]=0x00;
-			resp[3]=0x00;
-			bc = 4;
-			break;
-		case 188: /* BC Write EEPROM */
-			resp[0] = 0;
-			bc = 1;
-		case 189: /* BD Read TX Status */
-			resp[0] = 0;
-			bc = 1;
-		case 231: /* E7 */
-			resp[0] = 0x09; // S9 signal;
-			bc = 1;
-			break;
-		case 247: /* F7 */
-			resp[0]=0x00;
-			bc = 1;
-			break;
-		case 255: /* FF sent out by HRD */
-			break;
-		// default:
-			// while (1);
-		}
+		cat_driver_put_data(resp,bc);
+		/* Return data back */
 	}
-	cat_driver_put_data(resp,bc);
-	/* Return data back */
 }
 
