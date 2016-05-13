35e8297c5ab35fc6ab9a2f7152d34aef4e9becbb
df8oe <df8oe@gmx.de>
2016-05-13 15:41:18 +0200

    preparing next devel

--- mchf-eclipse/hardware/mchf_board.h
+++ mchf-eclipse/hardware/mchf_board.h
@@ -49,7 +49,7 @@
 #define 	TRX4M_VER_MINOR			219
 #define 	TRX4M_VER_RELEASE		27
 //
-#define 	TRX4M_VER_BUILD			17
+#define 	TRX4M_VER_BUILD			18
 //
 
 #define		ATTRIB_STRING1			"Additional Contributions by"
