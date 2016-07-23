1f34896efc89a86f020285171f1cb2dd4a7bea3c
dd4wh <dd4wh.swl@gmail.com>
2016-07-23 21:26:01 +0200

    now FIR filters for TX path can be adjusted

--- mchf-eclipse/drivers/audio/audio_filter.c
+++ mchf-eclipse/drivers/audio/audio_filter.c
@@ -986,10 +986,21 @@ void AudioFilter_InitTxHilbertFIR(void)
     fc.tx_q_block_size = Q_TX_BLOCK_SIZE;
     fc.tx_i_block_size = I_TX_BLOCK_SIZE;
     //
-    for(i = 0; i < Q_TX_NUM_TAPS; i++)
+    if(ts.tx_filter == TX_FILTER_WIDE_BASS || ts.tx_filter == TX_FILTER_WIDE_TREBLE)
     {
-        fc.tx_filt_q[i] = q_tx_coeffs[i];
-        fc.tx_filt_i[i] = i_tx_coeffs[i];
+        for(i = 0; i < Q_TX_NUM_TAPS; i++)
+            {
+                fc.tx_filt_q[i] = q_tx_wide_coeffs[i];
+                fc.tx_filt_i[i] = i_tx_wide_coeffs[i];
+            }
+    }
+    else
+    {
+        for(i = 0; i < Q_TX_NUM_TAPS; i++)
+        	{
+            	fc.tx_filt_q[i] = q_tx_coeffs[i];
+            	fc.tx_filt_i[i] = i_tx_coeffs[i];
+        	}
     }
 
     arm_fir_init_f32((arm_fir_instance_f32 *)&FIR_I_TX,fc.tx_i_num_taps,(float32_t *)&fc.tx_filt_i[0], &FirState_I_TX[0],fc.tx_i_block_size);
