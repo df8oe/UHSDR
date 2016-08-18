8ee804f10e8ba8abea8f038710634edabcc7cb74
Michael DL2FW <dl2fw@gmx.de>
2016-08-18 07:55:23 +0200

    FreeDV txt message activated and output power increased (empirical)

--- mchf-eclipse/drivers/audio/audio_driver.c
+++ mchf-eclipse/drivers/audio/audio_driver.c
@@ -3324,7 +3324,7 @@ static void audio_dv_tx_processor (AudioSample_t * const src, AudioSample_t * co
 #endif
 
         // apply I/Q amplitude & phase adjustments
-        audio_tx_final_iq_processing(0.35, ts.dmod_mode == DEMOD_LSB, dst, blockSize);
+        audio_tx_final_iq_processing(2.0*SSB_GAIN_COMP, ts.dmod_mode == DEMOD_LSB, dst, blockSize);
     }
     else
     {
