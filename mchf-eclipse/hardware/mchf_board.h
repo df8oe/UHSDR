e28309717171f251640b505e39d2fb9cc9782fbc
df8oe <df8oe@gmx.de>
2016-08-20 08:58:02 +0200

    preparing next devel stage - fix error

--- mchf-eclipse/hardware/mchf_board.h
+++ mchf-eclipse/hardware/mchf_board.h
@@ -63,8 +63,8 @@
 #define 	AUTHOR_STRING   		"K. Atanassov - M\x60NKA 2014-2016"
 //
 #define 	TRX4M_VER_MAJOR			1
-#define 	TRX4M_VER_MINOR			2
-#define 	TRX4M_VER_RELEASE		1
+#define 	TRX4M_VER_MINOR			3
+#define 	TRX4M_VER_RELEASE		0
 //
 #define		ATTRIB_STRING1			"Additional Contributions by"
 #define		ATTRIB_STRING2			"KA7OEI, DF8OE and others."
