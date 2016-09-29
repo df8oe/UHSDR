e87c67c7c6d2cfb398e78a7b2761f4afac7998c8
db4ple <db4ple@github.com>
2016-09-29 21:44:52 +0200

    Nothing

--- mchf-eclipse/hardware/mchf_board.h
+++ mchf-eclipse/hardware/mchf_board.h
@@ -17,8 +17,8 @@
 // some special switches
 //#define 	DEBUG_BUILD
 
-// #define USE_FREEDV //uncomment to use freedv instead of SNAP function
-// #define DEBUG_FREEDV
+//#define USE_FREEDV //uncomment to use freedv instead of SNAP function
+//#define DEBUG_FREEDV
 // hardware specific switches
 //#define HY28BHISPEED			true		// uncomment for using new HY28B in SPI with bus speed 50MHz instead of 25MHz
 
