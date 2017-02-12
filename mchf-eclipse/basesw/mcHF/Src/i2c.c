2e4b609e2bbd9031a95f96906b08722046bb7c50
Dummy <dummy@epost.de>
2017-02-12 15:40:42 +0100

    Added original address (0x33)

--- mchf-eclipse/basesw/mcHF/Src/i2c.c
+++ mchf-eclipse/basesw/mcHF/Src/i2c.c
@@ -61,7 +61,7 @@ void MX_I2C1_Init(void)
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 200000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_16_9;
-  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.OwnAddress1 = 102;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
