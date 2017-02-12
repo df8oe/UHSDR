c09fe865f843551446c217568247850bc3dbc6d5
Dummy <dummy@epost.de>
2017-02-12 21:23:46 +0100

    I2C2 Slow Down

--- mchf-eclipse/basesw/mcHF/Src/i2c.c
+++ mchf-eclipse/basesw/mcHF/Src/i2c.c
@@ -78,7 +78,7 @@ void MX_I2C2_Init(void)
 {
 
   hi2c2.Instance = I2C2;
-  hi2c2.Init.ClockSpeed = 200000;
+  hi2c2.Init.ClockSpeed = 100000;
   hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_16_9;
   hi2c2.Init.OwnAddress1 = 102;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
