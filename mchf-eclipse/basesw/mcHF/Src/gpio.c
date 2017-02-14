541dcd73700d57e97ec2d1f6abccbc01307ba815
Dummy <dummy@epost.de>
2017-02-14 07:52:42 +0100

    Fixed edge trigger mode

--- mchf-eclipse/basesw/mcHF/Src/gpio.c
+++ mchf-eclipse/basesw/mcHF/Src/gpio.c
@@ -92,7 +92,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PEPin PEPin */
   GPIO_InitStruct.Pin = PADDLE_DAH_Pin|PADDLE_DIT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
