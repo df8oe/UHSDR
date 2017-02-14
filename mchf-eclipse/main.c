d4d8313d11078e88665331943e9cf421b7a27cf8
dd4wh <dd4wh.swl@gmail.com>
2017-02-14 17:02:39 +0100

    AGC WDSP more user-freindly start values

--- mchf-eclipse/main.c
+++ mchf-eclipse/main.c
@@ -440,8 +440,8 @@ void TransceiverStateInit(void)
     ts.agc_wdsp_mode = 2;
     ts.agc_wdsp_slope = 70;
     ts.agc_wdsp_hang_enable = 0;
-    ts.agc_wdsp_hang_time = 250;
-    ts.agc_wdsp_hang_thresh = 85;
+    ts.agc_wdsp_hang_time = 500;
+    ts.agc_wdsp_hang_thresh = 45;
     ts.agc_wdsp_thresh = 60;
     ts.agc_wdsp_action = 0;
     ts.agc_wdsp_switch_mode = 1;
