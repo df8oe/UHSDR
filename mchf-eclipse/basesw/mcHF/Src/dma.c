e9f950672001b69722a940c950349a1083fc57b3
Dummy <dummy@epost.de>
2017-03-07 20:37:07 +0100

    Changed IRQ Priorities Reason: Digital In TX is now uninterrupted when using higher loads cause very long audio driver runtimes.

--- mchf-eclipse/basesw/mcHF/Src/dma.c
+++ mchf-eclipse/basesw/mcHF/Src/dma.c
@@ -66,13 +66,13 @@ void MX_DMA_Init(void)
 
   /* DMA interrupt init */
   /* DMA1_Stream2_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream2_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Stream2_IRQn, 2, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream2_IRQn);
   /* DMA1_Stream4_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream4_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Stream4_IRQn, 15, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream4_IRQn);
   /* DMA1_Stream5_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 15, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 
 }
