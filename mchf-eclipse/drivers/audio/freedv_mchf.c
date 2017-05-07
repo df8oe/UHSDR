36fb1c198b72ded7a2eedb5ada5b2f5b5810e873
dd4wh <dd4wh.swl@gmail.com>
2017-05-07 18:06:35 +0200

    solve rebase conflict 2

--- mchf-eclipse/drivers/audio/freedv_mchf.c
+++ mchf-eclipse/drivers/audio/freedv_mchf.c
@@ -691,11 +691,13 @@ void do_alternate_NR(float32_t* inputsamples, float32_t* outputsamples )
 
     for (int k=0; k < NR_FFT_SIZE;  k++)
                                     {
-                                        outputsamples[k] = inputsamples[k] + 100 * sawcount;// overlay sawtooth
+//                                        outputsamples[k] = inputsamples[k] + 100 * sawcount;// overlay sawtooth
                                         //outputsamples[k] = inputsamples[k];  just copy back
 
-                                        sawcount++;
-                                        if (sawcount > 15) sawcount=0;
+//                                        sawcount++;
+//                                        if (sawcount > 15) sawcount=0;
+// 1.
+
                                     }
 
 
