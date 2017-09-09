b84a7cd26a558a9414949b390bb05dbfc4f2b803
db4ple <db4ple@github.com>
2017-09-09 14:23:18 +0200

    Experimental CW decoder hang fix and other fixes. - Externalized cw_decoder_config to use it for external configuration - Refactored the CW decoder a little as part of the problem seeking mission - Found a minor bug in the original implementation arduino code (non-fatal, minor) - Found the problem with the endless loop caused by DataRecognition not announcing the final processing correctly. Now DataRecognition should never cause an endless loop in ErrorCorrection, and we are safe to use it even in the interrupt, since the number of iterations is bound by CW_SIG_BUFSIZE.

--- mchf-eclipse/drivers/audio/cw/cw_decoder.h
+++ mchf-eclipse/drivers/audio/cw/cw_decoder.h
@@ -8,6 +8,16 @@
 #ifndef AUDIO_CW_CW_DECODER_H_
 #define AUDIO_CW_CW_DECODER_H_
 
+typedef struct
+{
+	float32_t sampling_freq;
+	float32_t target_freq;
+	float32_t speed;
+} cw_config_t;
+
+extern cw_config_t cw_decoder_config;
+
+
 void CwDecode_RxProcessor(float32_t * const src, int16_t blockSize);
 void CwDecode_FilterInit();
 
