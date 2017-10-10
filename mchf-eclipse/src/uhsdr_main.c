dba123c496f24a1dd10f5c467bae2458aafd3e9c
DF8OE <df8oe@gmx.de>
2017-10-10 11:31:28 +0200

    yes - it IS (char*)...

--- mchf-eclipse/src/uhsdr_main.c
+++ mchf-eclipse/src/uhsdr_main.c
@@ -350,8 +350,7 @@ void TransceiverStateInit(void)
     for (int idx = 0; idx<KEYER_BUTTONS; idx++)
     {
     	ts.keyer_mode.macro[idx][0] = '\0';
-    	strcpy(ts.keyer_mode.cap[idx], "BTN");
-
+    	strcpy((char*)ts.keyer_mode.cap[idx], "BTN");
     }
     ts.buffered_tx = false;
     ts.cw_text_entry = false;
