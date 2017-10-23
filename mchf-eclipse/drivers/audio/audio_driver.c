9195f006875f94e697de1e920c5727cd126908da
dd4wh <dd4wh.swl@gmail.com>
2017-10-23 19:31:45 +0200

    leaky LMS NR prep#1

--- mchf-eclipse/drivers/audio/audio_driver.c
+++ mchf-eclipse/drivers/audio/audio_driver.c
@@ -3412,6 +3412,84 @@ static void AudioDriver_RxHandleIqCorrection(const uint16_t blockSize)
     }
 
 }
+
+
+#if 0
+// Automatic noise reduction
+// Variable-leak LMS algorithm
+// taken from (c) Warren Pratts wdsp library 2016
+// GPLv3 licensed
+#define ANR_DLINE_SIZE 256 //512 //2048 funktioniert nicht, 128 & 256 OK                 // dline_size
+int ANR_taps =     64; //64;                       // taps
+int ANR_delay =    16; //16;                       // delay
+int ANR_dline_size = ANR_DLINE_SIZE;
+//int ANR_buff_size = FFT_length / 2.0;
+int ANR_position = 0;
+float32_t ANR_two_mu =   0.0001;                     // two_mu --> "gain"
+float32_t ANR_gamma =    0.1;                      // gamma --> "leakage"
+float32_t ANR_lidx =     120.0;                      // lidx
+float32_t ANR_lidx_min = 0.0;                      // lidx_min
+float32_t ANR_lidx_max = 200.0;                      // lidx_max
+float32_t ANR_ngamma =   0.001;                      // ngamma
+float32_t ANR_den_mult = 6.25e-10;                   // den_mult
+float32_t ANR_lincr =    1.0;                      // lincr
+float32_t ANR_ldecr =    3.0;                     // ldecr
+int ANR_mask = ANR_dline_size - 1;
+int ANR_in_idx = 0;
+float32_t ANR_d [ANR_DLINE_SIZE];
+float32_t ANR_w [ANR_DLINE_SIZE];
+uint8_t ANR_on = 0;
+uint8_t ANR_notch = 0;
+
+void AudioDriver_LeakyLmsNr (float32_t *ANR_in_buff, float32_t *ANR_out_buff, int ANR_buff_size)
+{
+    int i, j, idx;
+    float32_t c0, c1;
+    float32_t y, error, sigma, inv_sigp;
+    float32_t nel, nev;
+		for (i = 0; i < ANR_buff_size; i++)
+		{
+			ANR_d[ANR_in_idx] = ANR_in_buff[i];
+
+			y = 0;
+			sigma = 0;
+
+			for (j = 0; j < ANR_n_taps; j++)
+			{
+				idx = (ANR_in_idx + j + ANR_delay) & ANR_mask;
+				y += ANR_w[j] * ANR_d[idx];
+				sigma += ANR_d[idx] * ANR_d[idx];
+			}
+			inv_sigp = 1.0 / (sigma + 1e-10);
+			error = ANR_d[ANR_in_idx] - y;
+
+			ANR_out_buff[i] = y;
+//			ANR_out_buff[2 * i + 1] = 0.0;
+
+			if((nel = error * (1.0 - ANR_two_mu * sigma * inv_sigp)) < 0.0) nel = -nel;
+			if((nev = ANR_d[ANR_in_idx] - (1.0 - ANR_two_mu * ANR_ngamma) * y - ANR_two_mu * error * sigma * inv_sigp) < 0.0) nev = -nev;
+			if (nev < nel)
+				if((ANR_lidx += ANR_lincr) > ANR_lidx_max) ANR_lidx = ANR_lidx_max;
+			else
+				if((ANR_lidx -= ANR_ldecr) < ANR_lidx_min) ANR_lidx = ANR_lidx_min;
+			ANR_ngamma = ANR_gamma * (ANR_lidx * ANR_lidx) * (ANR_lidx * ANR_lidx) * ANR_den_mult;
+
+			c0 = 1.0 - ANR_two_mu * ANR_ngamma;
+			c1 = ANR_two_mu * error * inv_sigp;
+
+			for (j = 0; j < ANR_n_taps; j++)
+			{
+				idx = (ANR_in_idx + j + ANR_delay) & ANR_mask;
+				ANR_w[j] = c0 * ANR_w[j] + c1 * ANR_d[idx];
+			}
+			ANR_in_idx = (ANR_in_idx + ANR_mask) & ANR_mask;
+		}
+}
+
+#endif
+
+
+
 //
 //*----------------------------------------------------------------------------
 //* Function Name       : audio_rx_processor
