8c8583e3956c62e739b2336c272b0f9a14d63307
DF8OE <df8oe@gmx.de>
2017-10-23 19:57:20 +0200

    releasing new daily build

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -16,7 +16,7 @@
 #define 	UHSDR_VER_MINOR			"5"
 #define 	UHSDR_VER_RELEASE		"127"
 
-#define		UHSDR_BOOT_VERS			"3.4.1"
+#define		UHSDR_BOOT_VERS			"3.4.2"
 
 
 #ifdef SBLA									// Bootloader enables additional rf bands PCB
