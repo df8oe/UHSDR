507d63ac646c0cc4b4756525149c7a03aa111a11
DF8OE <df8oe@gmx.de>
2017-11-12 13:55:48 +0100

    starting of new development version

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -13,7 +13,7 @@
 ************************************************************************************/
 
 #define 	UHSDR_VER_MAJOR			"2"
-#define 	UHSDR_VER_MINOR			"6"
+#define 	UHSDR_VER_MINOR			"7"
 #define 	UHSDR_VER_RELEASE		"0"
 
 #define		UHSDR_BOOT_VERS			"3.4.2"
