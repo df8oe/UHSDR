57f4662e6a993c025b9de1d560a370d0f600a8ad
Richard <33404417+quadrant2005@users.noreply.github.com>
2017-12-12 20:15:16 +0000

    Add files via upload
    test

---
+++ uhsdr_version.h
@@ -0,0 +1,35 @@
+/*  -*-  mode: c; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4; coding: utf-8  -*-  */
+/************************************************************************************
+ **                                                                                 **
+ **                                        UHSDR                                    **
+ **               a powerful firmware for STM32 based SDR transceivers              **
+ **                                                                                 **
+ **---------------------------------------------------------------------------------**
+ **                                                                                 **
+ **  File name:                                                                     **
+ **  Description:                                                                   **
+ **  Last Modified:                                                                 **
+ **  Licence:		GNU GPLv3                                                      **
+ ************************************************************************************/
+
+#define 	UHSDR_VER_MAJOR			"2"
+#define 	UHSDR_VER_MINOR			"7"
+#define 	UHSDR_VER_RELEASE		"42Q"
+//[QBS] this is an unofficial experimental spin of of the MCHF UHSDR firmware
+#define		UHSDR_BOOT_VERS			"3.4.4"
+
+
+#ifdef SBLA									// Bootloader enables additional rf bands PCB
+  #define	UHSDR_BOOT_VER			UHSDR_BOOT_VERS"a"
+#elif SBLS									// Bootloader enables special debug capabilities
+  #define	UHSDR_BOOT_VER			UHSDR_BOOT_VERS"s"
+#else										// Standard Bootloader
+  #define	UHSDR_BOOT_VER			UHSDR_BOOT_VERS
+#endif
+
+// trailing characters are needed for identifying version and building date+time in binary
+#define	    UHSDR_VERSION           "fwv-"UHSDR_VER_MAJOR"."UHSDR_VER_MINOR"."UHSDR_VER_RELEASE
+#define	    UHSDR_BUILD_DAT         "fwt-"__DATE__ " - " __TIME__
+#define	    UHSDR_LICENCE           "GNU GPLv3"
+#define     UHSDR_REPO              "github.com/quadrant2005/MCHF-MEGA"
+//https://github.com/quadrant2005/MCHF-MEGA.git
