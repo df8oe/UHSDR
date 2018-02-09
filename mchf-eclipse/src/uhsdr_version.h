770c6171d809ddcaede45ac62ba1319e01ec65fe
DF8OE <df8oe@gmx.de>
2018-02-09 18:40:57 +0100

    releasing new stable build of firmware version 2.8.0

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -13,10 +13,10 @@
  ************************************************************************************/
 
 #define 	UHSDR_VER_MAJOR			"2"
-#define 	UHSDR_VER_MINOR			"7"
-#define 	UHSDR_VER_RELEASE		"96"
+#define 	UHSDR_VER_MINOR			"8"
+#define 	UHSDR_VER_RELEASE		"0"
 
-#define		UHSDR_BOOT_VERS			"3.4.6"
+#define		UHSDR_BOOT_VERS			"4.0.0"
 
 
 #ifdef SBLA									// Bootloader enables additional rf bands PCB
