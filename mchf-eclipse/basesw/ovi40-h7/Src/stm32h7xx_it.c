d7d8609cd54b43cd5d875e5ea9bab8da85582227
db4ple <db4ple@github.com>
2018-08-27 20:44:57 +0200

    Fix for #1568 (No NB or NR on H7) In fact also FreeDV did not work. It was a simple forgotten necessary copy and past issue in the machine specific part...

--- mchf-eclipse/basesw/ovi40-h7/Src/stm32h7xx_it.c
+++ mchf-eclipse/basesw/ovi40-h7/Src/stm32h7xx_it.c
@@ -152,7 +152,7 @@ void DebugMon_Handler(void)
 void PendSV_Handler(void)
 {
   /* USER CODE BEGIN PendSV_IRQn 0 */
-
+    UiDriver_TaskHandler_HighPrioTasks();
   /* USER CODE END PendSV_IRQn 0 */
   /* USER CODE BEGIN PendSV_IRQn 1 */
 
