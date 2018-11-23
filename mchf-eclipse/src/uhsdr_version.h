51824ab9a77b9e0f529a3f168a59eff0518ae0ce
DF8OE <df8oe@gmx.de>
2018-11-23 06:40:51 +0100

    releasing new daily build of bootloader version 5.0.0

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -16,7 +16,7 @@
 #define 	UHSDR_VER_MINOR			"9"
 #define 	UHSDR_VER_RELEASE		"73"
 
-#define		UHSDR_BOOT_VERS			"4.0.0"
+#define		UHSDR_BOOT_VERS			"5.0.0"
 
 
 #ifdef SBLA									// Bootloader enables additional rf bands PCB
