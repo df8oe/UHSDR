839484d0559c2541745461a523225649806dec56
DF8OE <df8oe@gmx.de>
2018-12-31 09:18:43 +0100

    release of new stable version

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -13,8 +13,8 @@
  ************************************************************************************/
 
 #define 	UHSDR_VER_MAJOR			"2"
-#define 	UHSDR_VER_MINOR			"9"
-#define 	UHSDR_VER_RELEASE		"92"
+#define 	UHSDR_VER_MINOR			"10"
+#define 	UHSDR_VER_RELEASE		"0"
 
 #define		UHSDR_BOOT_VERS			"5.0.2"
 
