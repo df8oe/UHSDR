e8f705283e45c07f8f09436e91ad9af410ec58eb
db4ple <db4ple@users.noreply.github.com>
2019-08-11 09:52:06 +0200

    Update radio_management.c

--- mchf-eclipse/drivers/ui/radio_management.c
+++ mchf-eclipse/drivers/ui/radio_management.c
@@ -364,10 +364,10 @@ static bool RadioManagement_SetBandPowerFactor(const BandInfo* band, int32_t pow
     {
         // TX outside bands **very dirty hack**
         //  FIXME: calculate based on 2 frequency points close the selected frequency, should be inter-/extrapolated
-        uint32_t freq_min = bandInfo[BAND_MODE_80].tune;
+        uint32_t freq_min = RadioManagement_GetBandInfo(BAND_MODE_80)->tune;
         float32_t adj_min = ts.pwr_adj[ADJ_REF_PWR][BAND_MODE_80] / RadioManagement_IsPowerFactorReduce(freq_min)? 400: 100;
 
-        uint32_t freq_max = bandInfo[BAND_MODE_10].tune;
+        uint32_t freq_max = RadioManagement_GetBandInfo(BAND_MODE_10)->tune;
         float32_t adj_max = ts.pwr_adj[ADJ_REF_PWR][BAND_MODE_10] / RadioManagement_IsPowerFactorReduce(freq_max)? 400: 100;
 
         float32_t delta_f = (float32_t)df.tune_old - (float32_t)freq_min; // we must convert to a signed type
@@ -379,7 +379,7 @@ static bool RadioManagement_SetBandPowerFactor(const BandInfo* band, int32_t pow
     }
     else
     {
-        pf_bandvalue = ts.pwr_adj[power == 0?ADJ_FULL_POWER:ADJ_REF_PWR][band];
+        pf_bandvalue = ts.pwr_adj[power == 0?ADJ_FULL_POWER:ADJ_REF_PWR][band->band_mode];
         pf_bandvalue /= RadioManagement_IsPowerFactorReduce(df.tune_old)? 400: 100;
     }
 
