dc2d1815609cb15bf9c006056867d423f74a81bf
DF8OE <df8oe@gmx.de>
2021-03-07 14:56:47 +0100

    started new unstable version

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -13,8 +13,8 @@
  ************************************************************************************/
 
 #define 	UHSDR_VER_MAJOR			"2"
-#define 	UHSDR_VER_MINOR			"11"
-#define 	UHSDR_VER_RELEASE		"95"
+#define 	UHSDR_VER_MINOR			"13"
+#define 	UHSDR_VER_RELEASE		"1"
 
 #define		UHSDR_BOOT_VERS			"5.0.4"
 
