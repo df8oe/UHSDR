34e8f846a93246257383815ea7a8f1a1d25650cf
DF8OE <df8oe@gmx.de>
2021-03-08 06:19:55 +0100

    releasing new stable build of firmware version 2.12.0

--- mchf-eclipse/src/uhsdr_version.h
+++ mchf-eclipse/src/uhsdr_version.h
@@ -13,8 +13,8 @@
  ************************************************************************************/
 
 #define 	UHSDR_VER_MAJOR			"2"
-#define 	UHSDR_VER_MINOR			"11"
-#define 	UHSDR_VER_RELEASE		"96"
+#define 	UHSDR_VER_MINOR			"12"
+#define 	UHSDR_VER_RELEASE		"0"
 
 #define		UHSDR_BOOT_VERS			"5.0.4"
 
